0275118b81e0ac2e100d3f3e26d1fd754658c695
Jacob Moore <jmooree30@gmail.com>
2018-09-02 22:57:42 -0400

    Login logic/JWT complete

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 const express = require("express");
 const mongoose = require("mongoose");
-
+const bodyParcer = require("body-parser");
 const users = require("./routes/api/users");
 const posts = require("./routes/api/posts");
 const profile = require("./routes/api/profile");
 
 const app = express();
+
+app.use(bodyParcer.urlencoded({ extended: false }));
+app.use(bodyParcer.json());
 
 const db = require("./config/keys").mongoURI;
 
