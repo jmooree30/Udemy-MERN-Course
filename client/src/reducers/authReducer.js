8a49a61fa602483fe1bcb94a7f3d83cf2140aff9
Jacob Moore <jmooree30@gmail.com>
2018-09-13 22:58:34 -0400

    Login and Signup complete (Refer to me in the future)

--- client/src/reducers/authReducer.js
+++ client/src/reducers/authReducer.js
@@ -1,3 +1,7 @@
+import { SET_CURRENT_USER } from "../actions/types";
+import { SSL_OP_DONT_INSERT_EMPTY_FRAGMENTS } from "constants";
+import isEmpty from "../validation/is-empty";
+
 const initalState = {
   isAuthenticated: false,
   user: {}
@@ -5,6 +9,12 @@ const initalState = {
 
 export default function(state = initalState, action) {
   switch (action.type) {
+    case SET_CURRENT_USER:
+      return {
+        ...state,
+        isAuthenticated: !isEmpty(action.payload),
+        user: action.payload
+      };
     default:
       return state;
   }
